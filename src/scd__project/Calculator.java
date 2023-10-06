a9e1864319c319625cc1ec4ebeb21f09f7d8db0b
Zohaib Yasin <zohaiby33@gmail.com>
2023-10-06 10:34:05 +0500

    F21-9658 adding calculator funcationality

---
+++ src/scd__project/Calculator.java
@@ -0,0 +1,11 @@
+package scd__project;
+
+public class Calculator {
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+}
